58d688a4543dcf33aab1b8eeec857d6de906ad90
Diego Nobre <diegonobre.dev@outlook.com>
2023-09-13 11:53:42 -0300

    Implementação calculo hidratação isotonico/venosa

--- src/app/pages/hidratacao-venosa/hidratacao-venosa.component.ts
+++ src/app/pages/hidratacao-venosa/hidratacao-venosa.component.ts
@@ -29,4 +29,60 @@ export class HidratacaoVenosaComponent {
       }
     });
   }
+
+
+  peso: any;
+  etapas: any;
+  valueSG5: any;
+  valueNaCl20: any;
+  valueNaCl20Tonico: any;
+  valueKCl10: any;
+  etapaSG5: any;
+  etapaNaCl20: any;
+  etapaNaCl20Tonico: any;
+  etapaKCl10: any;
+  taxaInfusaoBombaVenosa: any;
+  taxaInfusaoGotasMinVenosa: any;
+  taxaInfusaoMicrogotasMinVenosa:any;
+  taxaInfusaoBombaIsotonica: any;
+  taxaInfusaoGotasMinIsotonica: any;
+  taxaInfusaoMicrogotasMinIsotonica:any;
+  
+  back(){
+    history.back()
+  }
+
+  calcularHidratacaoVenosa(){
+    if(this.peso <= 10){
+      this.valueSG5 = 100;
+      this.valueNaCl20 = 3;
+      this.valueKCl10 = 2;
+      this.valueNaCl20Tonico = this.valueSG5 * 0.0411;
+    }
+    if(this.peso > 10 && this.peso <= 20){
+      this.valueSG5 = 1050;
+      this.valueNaCl20 = 32;
+      this.valueKCl10 = 21;
+      this.valueNaCl20Tonico = this.valueSG5 * 0.0411;
+    }
+    if(this.peso > 20){
+      this.valueSG5 = (1500 + (20 * (this.peso - 20)));
+      this.valueNaCl20 = (50 + (1 * (this.peso - 20))) / 3.4;
+      this.valueKCl10 = (30 + (0.5 * (this.peso - 20))) / 1.3;
+      this.valueNaCl20Tonico = this.valueSG5 * 0.0411;
+    }
+
+    this.etapaSG5 = (this.valueSG5/this.etapas).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+    this.etapaNaCl20 = (this.valueNaCl20/this.etapas).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+    this.etapaKCl10 = (this.valueKCl10/this.etapas).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+    this.etapaNaCl20Tonico = (this.valueNaCl20Tonico/this.etapas).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+
+    this.taxaInfusaoBombaVenosa = ((this.valueSG5 + this.valueNaCl20 + this.valueKCl10) / 24).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+    this.taxaInfusaoGotasMinVenosa = ((this.valueSG5 + this.valueNaCl20 + this.valueKCl10) / 72).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+    this.taxaInfusaoMicrogotasMinVenosa = ((this.valueSG5 + this.valueNaCl20 + this.valueKCl10) / 24).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+   
+    this.taxaInfusaoBombaIsotonica = ((this.valueSG5 + this.valueNaCl20Tonico + this.valueKCl10) / 24).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+    this.taxaInfusaoGotasMinIsotonica = ((this.valueSG5 + this.valueNaCl20Tonico + this.valueKCl10) / 72).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+    this.taxaInfusaoMicrogotasMinIsotonica = ((this.valueSG5 + this.valueNaCl20Tonico + this.valueKCl10) / 24).toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+  }
 }
