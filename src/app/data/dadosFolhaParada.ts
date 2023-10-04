6f27b23a1bda5156642c1c0cd983b2a2eac5601b
Diego Nobre <diegonobre.dev@outlook.com>
2023-10-04 15:37:28 -0300

    Ajuste Dados

---
+++ src/app/data/dadosFolhaParada.ts
@@ -0,0 +1,253 @@
+export const bolsasValvaRessuscitacao = [
+  {value: 1, label: "Lactente"},
+  {value: 2, label: "Lactente/criança"},
+  {value: 3, label: "Lactente/criança"},
+  {value: 4, label: "Criança"},
+  {value: 5, label: "Criança"},
+  {value: 6, label: "Criança"},
+  {value: 7, label: "Criança"},
+  {value: 8, label: "Criança/adulto"},
+  {value: 9, label: "Adulto"}
+];
+
+export const mascarasO2 = [
+  {value: 1, label: "Neonatal"},
+  {value: 2, label: "Neonatal"},
+  {value: 3, label: "Neonatal"},
+  {value: 4, label: "Pediátrica"},
+  {value: 5, label: "Pediátrica"},
+  {value: 6, label: "Pediátrica"},
+  {value: 7, label: "Pediátrica"},
+  {value: 8, label: "Adulto"},
+  {value: 9, label: "Adulto"}
+];
+
+
+export const canulasOroFaringea = [
+  {value: 1, label: "0"},
+  {value: 2, label: "0-1"},
+  {value: 3, label: "1"},
+  {value: 4, label: "1"},
+  {value: 5, label: "1-2"},
+  {value: 6, label: "2"},
+  {value: 7, label: "2"},
+  {value: 8, label: "2-3"},
+  {value: 9, label: "3 ou +"}
+];
+
+export const laminasLaringoscopio = [
+  {value: 1, label: "Reta 0-1"},
+  {value: 2, label: "Reta 1"},
+  {value: 3, label: "Reta 1"},
+  {value: 4, label: "Reta 1"},
+  {value: 5, label: "Reta 2"},
+  {value: 6, label: "Reta 2"},
+  {value: 7, label: "Reta 2 ou curva"},
+  {value: 8, label: "Reta 2-3 ou curva"},
+  {value: 9, label: "Reta 3 ou curva"}
+];
+
+export const canulasTraqueal = [
+  {value: 1, label: "Prematuro 2,5 Termo 3,0 - 3,5 sem cuff"},
+  {value: 2, label: "3,5 sem cuff - 3,0 com cuff"},
+  {value: 3, label: "3,5 sem cuff - 3,0 com cuff"},
+  {value: 4, label: "4,0 sem cuff - 3,5 com cuff"},
+  {value: 5, label: "4,5 sem cuff - 4,0 com cuff"},
+  {value: 6, label: "5,0 sem cuff - 4,5 com cuff"},
+  {value: 7, label: "5,5 sem cuff - 5,0 com cuff"},
+  {value: 8, label: "6,0 com cuff"},
+  {value: 9, label: "6,5 com cuff"}
+];
+
+export const comprimentosCanulaTraqueal = [
+  {value: 1, label: "3kg: 9-9,5 / 4kg: 9,5-10 / 5kg: 10-10,5"},
+  {value: 2, label: "10,5 - 11"},
+  {value: 3, label: "10,5 - 11"},
+  {value: 4, label: "11 - 12"},
+  {value: 5, label: "12,5 - 13,5"},
+  {value: 6, label: "14 - 15"},
+  {value: 7, label: "15,5 - 16,5"},
+  {value: 8, label: "17 - 18"},
+  {value: 9, label: "18,5 - 19,5"}
+];
+
+export const fioGuias = [
+  {value: 1, label: "6"},
+  {value: 2, label: "6"},
+  {value: 3, label: "6"},
+  {value: 4, label: "6"},
+  {value: 5, label: "6"},
+  {value: 6, label: "6"},
+  {value: 7, label: "14"},
+  {value: 8, label: "14"},
+  {value: 9, label: "14"}
+];
+
+export const sondasAspiracao = [
+  {value: 1, label: "6 - 8"},
+  {value: 2, label: "6 - 8"},
+  {value: 3, label: "8"},
+  {value: 4, label: "8 - 10"},
+  {value: 5, label: "10"},
+  {value: 6, label: "10"},
+  {value: 7, label: "10"},
+  {value: 8, label: "10"},
+  {value: 9, label: "12"}
+];
+
+export const manguitosPA = [
+  {value: 1, label: "Neonato/lactente"},
+  {value: 2, label: "Neonato/lactente"},
+  {value: 3, label: "Neonato/lactente"},
+  {value: 4, label: "Lactente/criança"},
+  {value: 5, label: "Criança"},
+  {value: 6, label: "Criança"},
+  {value: 7, label: "Criança"},
+  {value: 8, label: "Criança"},
+  {value: 9, label: "Pequeno adulto"}
+];
+
+export const catetersEV = [
+  {value: 1, label: "22 - 24"},
+  {value: 2, label: "22 - 24"},
+  {value: 3, label: "22 - 24"},
+  {value: 4, label: "20 - 24"},
+  {value: 5, label: "18 - 22"},
+  {value: 6, label: "18 - 22"},
+  {value: 7, label: "18 - 20"},
+  {value: 8, label: "18 - 20"},
+  {value: 9, label: "16 - 20"}
+];
+
+export const intrasOssea = [
+  {value: 1, label: "18/15"},
+  {value: 2, label: "18/15"},
+  {value: 3, label: "18/15"},
+  {value: 4, label: "15"},
+  {value: 5, label: "15"},
+  {value: 6, label: "15"},
+  {value: 7, label: "15"},
+  {value: 8, label: "15"},
+  {value: 9, label: "15"}
+];
+
+export const sondasNasogastrica = [
+  {value: 1, label: "5 - 8"},
+  {value: 2, label: "5 - 8"},
+  {value: 3, label: "5 - 8"},
+  {value: 4, label: "8 - 10"},
+  {value: 5, label: "10"},
+  {value: 6, label: "10"},
+  {value: 7, label: "12 - 14"},
+  {value: 8, label: "14 - 18"},
+  {value: 9, label: "16 - 18"}
+];
+
+export const sondasUrinaria = [
+  {value: 1, label: "5"},
+  {value: 2, label: "5 - 8"},
+  {value: 3, label: "5 - 8"},
+  {value: 4, label: "8 - 10"},
+  {value: 5, label: "10"},
+  {value: 6, label: "10"},
+  {value: 7, label: "12 - 14"},
+  {value: 8, label: "14 - 18"},
+  {value: 9, label: "16 - 18"}
+];
+
+export const pasDesfibrilacoes = [
+  {value: 1, label: "Pás de lactente <1 ano"},
+  {value: 2, label: "Pás de lactente <1 ano"},
+  {value: 3, label: "Pás de lactente <1 ano ou 10kg"},
+  {value: 4, label: "Pás de lactente >= 1 ano ou >= 10kg"},
+  {value: 5, label: "Pás de adulto"},
+  {value: 6, label: "Pás de adulto"},
+  {value: 7, label: "Pás de adulto"},
+  {value: 8, label: "Pás de adulto"},
+  {value: 9, label: "Pás de adulto"}
+];
+
+export const drenosToracico = [
+  {value: 1, label: "10"},
+  {value: 2, label: "10 - 12"},
+  {value: 3, label: "10 - 12"},
+  {value: 4, label: "16 - 20"},
+  {value: 5, label: "20 - 24"},
+  {value: 6, label: "20 - 24"},
+  {value: 7, label: "24 - 32"},
+  {value: 8, label: "28 - 32"},
+  {value: 9, label: "32 - 38"}
+];
+
+export const mascarasLaringea = [
+  {value: 1, label: "1"},
+  {value: 2, label: "1 - 1,5"},
+  {value: 3, label: "1,5"},
+  {value: 4, label: "1,5"},
+  {value: 5, label: "2"},
+  {value: 6, label: "2"},
+  {value: 7, label: "2 - 2,5"},
+  {value: 8, label: "2,5"},
+  {value: 9, label: "3"}
+]; 
+
+
+
+export const MEDICAMENTOS = {
+    ADRENALINE_IV_IO: 0.01,
+    ADRENALINE_ET: 0.1,
+    ADRENALINE_IM: 0.01,
+    ATROPINE: 0.02,
+    AMIODARONE: 5,
+    ATP: 0.167,
+    CALCIUM_GLUCONATE: 0.5,//ml
+    DEXTROSE_D10: 2.5, //ml
+    DEXTROSE_D50: 0.5, //ml
+    FLUMAZENIL: 0.01,
+    LIGNOCAINE: 1,
+    LORAZEPAM: 0.1,
+    MAGNESIUM_SULFATE: 40,
+    NALOXONE: 0.1,
+    NAHCO3: 1, //ml
+    PROCANAMIDE: 15, 
+    KETAMINE: 1,
+    MIDAZOLAM: 0.1,
+    FENTANYL: 1, //mcg 
+    MORPHINE: 0.1, 
+    ETOMIDATE: 0.3,
+    PROPOFOL: 1, 
+    ROCURONIUM: 1, 
+    SUXAMETHONIUM: 1.5, 
+    NEOSTIGMINE: 0.05,
+    SUGAMMADEX: 16,
+    MgSO4_50:40,
+  };
+  
+  export const PREPARACOES = {
+    ADRENALINE_IV_IO: 0.1,
+    ADRENALINE_ET: 1,
+    ADRENALINE_IM: 0.1,
+    ATROPINE: 0.6,
+    AMIODARONE: 50,
+    ATP: 10,
+    CALCIUM_GLUCONATE: 0.5,
+    DEXTROSE_D10: 2.5,
+    DEXTROSE_D50: 0.5,
+    FLUMAZENIL: 0.1,
+    LIGNOCAINE: 20,
+    LORAZEPAM: 2,
+    MIDAZOLAM: 5,
+    MgSO4_50: 2500,
+    NALOXONE: 0.4,
+    NAHCO3: 1,
+    PROCANAMIDE: 100,
+    KETAMINE: 10,
+    FENTANYL: 50,
+    MORPHINE: 15,
+    ETOMIDATE: 2,
+    PROPOFOL: 10,
+    ROCURONIUM: 10,
+    SUXAMETHONIUM: 50,
+    NEOSTIGMINE: 2.5,
+    SUGAMMADEX: 100,
+  };
